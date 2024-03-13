0d0c5e51c9412a101cded5552439f58b92c8897c
Hryhorii_Struk <136825012+jnjijij@users.noreply.github.com>
2024-03-13 20:26:58 +0200

    Create models.py

---
+++ backend/autoria_clone/my_project/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Car(models.Model):
+    objects = None
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=3, choices=[('USD', 'USD'), ('EUR', 'EUR'), ('UAH', 'UAH')])
+    region = models.CharField(max_length=100)
+
+class CarPrice(models.Model):
+    objects = None
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=3, choices=[('USD', 'USD'), ('EUR', 'EUR'), ('UAH', 'UAH')])
+    date = models.DateField()
