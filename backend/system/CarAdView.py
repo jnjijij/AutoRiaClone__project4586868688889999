f0068bf8a222f9fe85248a69bb90c5648e32855d
Hryhorii_Struk <136825012+jnjijij@users.noreply.github.com>
2024-03-13 21:09:27 +0200

    Update CarAdView.py

--- backend/system/CarAdView.py
+++ backend/system/CarAdView.py
@@ -1,12 +1,10 @@
-class CarAd:
-    def __init__(self, car_id, region, price):
+class CarAdView:
+    def __init__(self, car_id, timestamp):
         self.car_id = car_id
-        self.region = region
-        self.price = price
+        self.timestamp = timestamp
 
     def to_dict(self):
         return {
             'car_id': self.car_id,
-            'region': self.region,
-            'price': self.price,
+            'timestamp': self.timestamp,
         }
