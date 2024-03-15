4a373a572fea43bd6107904cd7b4ba130d12e9d2
Lenovo <strukhr@gmail.com>
2024-03-15 14:24:48 +0200

    kk

--- backend/autoria/manage.py
+++ backend/autoria/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autoria.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autoria343.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
