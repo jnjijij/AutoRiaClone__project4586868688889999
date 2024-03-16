aaa69eb253a72959820fba1fefbbfc5bf59f2747
Lenovo <strukhr@gmail.com>
2024-03-16 12:32:59 +0200

    kk

--- backend/autoria_clone/page/models.py
+++ backend/autoria_clone/page/models.py
@@ -1,30 +1,39 @@
-from django import forms
-from models import CarBrand, CarModel, CarPrice, User, Ad
+from django.db import models
 
-class CarBrandForm(forms.ModelForm):
-    class Meta:
-        model = CarBrand
-        fields = ['name']
+class CarBrand(models.Model):
+    name = models.CharField(max_length=100)
 
-class CarModelForm(forms.ModelForm):
-    class Meta:
-        model = CarModel
-        fields = ['brand', 'name']
+    def __str__(self):
+        return self.name
 
-class CarPriceForm(forms.ModelForm):
-    class Meta:
-        model = CarPrice
-        fields = ['car', 'currency', 'price']
+class CarModel(models.Model):
+    brand = models.ForeignKey(CarBrand, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
 
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'account_type']
+    def __str__(self):
+        return self.name
 
-class AdForm(forms.ModelForm):
-    class Meta:
-        model = Ad
-        fields = ['user', 'car', 'price', 'content']
+class CarPrice(models.Model):
+    car = models.ForeignKey(CarModel, on_delete=models.CASCADE)
+    currency = models.CharField(max_length=3)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    account_type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+class Ad(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    car = models.ForeignKey(CarModel, on_delete=models.CASCADE)
+    price = models.ForeignKey(CarPrice, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return f"Ad for {self.car} by {self.user}"
 
 
 class CarBrand:
