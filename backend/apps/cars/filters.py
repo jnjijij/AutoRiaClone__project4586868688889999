8bbeda88139a5762390e38939936f12e75b38cbe
Lenovo <strukhr@gmail.com>
2024-03-17 09:39:05 +0200

    kk

--- backend/apps/cars/filters.py
+++ backend/apps/cars/filters.py
@@ -1,17 +1,15 @@
 from warnings import filters
 
-
 # CarModel.objects.filter(brand__icontains=)
-
 
 class CarFilter(filters.FilterSet):
     year_lt = filters.NumberFilter('year', 'lt')
     year_gt = filters.NumberFilter('year', 'gt')
     brand = filters.CharFilter('brand', 'icontains')
     order = filters.OrderingFilter(
-        fields=(
-            'id',
-            'brand',
-            ('year', 'asd')
-        )
+        fields=('id', 'brand', 'year', 'asd')
     )
+
+
+
+
--- backend/apps/cars/managers.py
+++ backend/apps/cars/managers.py
@@ -1,6 +1,17 @@
 from django.db import models
 
-
 class CarManager(models.Manager):
     def get_only_audi(self, name):
+        return self.filter(brand=name)
+
+    def get_only_bmw(self, name):
+        return self.filter(brand=name)
+
+    def get_only_mercedes(self, name):
+        return self.filter(brand=name)
+
+    def get_only_toyota(self, name):
+        return self.filter(brand=name)
+
+    def get_only_honda(self, name):
         return self.filter(brand=name)
