6074e0e8843ee1cb3e2bf10fec52eb26bebd6e1f
Hryhorii_Struk <136825012+jnjijij@users.noreply.github.com>
2024-03-18 17:27:15 +0200

    Update forms.py

--- backend/system/forms.py
+++ backend/system/forms.py
@@ -5,3 +5,11 @@ class CarForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = ['manufacturer', 'model', 'year', 'color']
+
+
+class UserProfileForm:
+    def save(self):
+        pass
+
+    def is_valid(self):
+        pass
