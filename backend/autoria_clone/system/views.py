e805a8559e755dfaa3cfc3ae2e9c15ef5b2a44a9
Hryhorii_Struk <136825012+jnjijij@users.noreply.github.com>
2024-03-18 16:59:56 +0200

    Create views.py

---
+++ backend/autoria_clone/system/views.py
@@ -0,0 +1,25 @@
+# backend/autoria_clone/system/views.py
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from .models import UserProfile
+from .forms import UserProfileForm
+
+User = get_user_model()
+
+@login_required
+def profile(request):
+    user_profile = UserProfile.objects.filter(user=request.user).first()
+    if not user_profile:
+        user_profile = UserProfile.objects.create(user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=user_profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserProfileForm(instance=user_profile)
+
+    return render(request, 'system/profile.html', {'form': form})
