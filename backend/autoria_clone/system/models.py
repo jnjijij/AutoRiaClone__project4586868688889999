28fc02adff66587e16716d08455c835a27f1caa4
Hryhorii_Struk <136825012+jnjijij@users.noreply.github.com>
2024-03-23 21:01:52 +0200

    Update models.py

--- backend/autoria_clone/system/models.py
+++ backend/autoria_clone/system/models.py
@@ -3,6 +3,7 @@
 from django.db import models
 
 class UserProfile(models.Model):
+    objects = None
     user = models.OneToOneField(
         'auth.User', on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(blank=True)
