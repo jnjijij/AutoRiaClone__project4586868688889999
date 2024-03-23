ee9db0f9515b731578cb3333723d6dc58e052fc3
Hryhorii_Struk <136825012+jnjijij@users.noreply.github.com>
2024-03-23 21:00:20 +0200

    Update models.py

--- backend/autoria/models.py
+++ backend/autoria/models.py
@@ -3,6 +3,7 @@
 from django.db import models
 
 class Car(models.Model):
+    objects = None
     make = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
     year = models.IntegerField()
@@ -12,3 +13,4 @@ class Car(models.Model):
 
     def __str__(self):
         return f"{self.make} {self.model} {self.year}"
+
