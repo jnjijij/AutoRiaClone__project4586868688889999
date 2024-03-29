e4fb4c00bc761f25803bb4f4abf36e48ddc720ab
Hryhorii_Struk <136825012+jnjijij@users.noreply.github.com>
2024-03-29 12:13:37 +0200

    Update urls.py

--- backend/apps/cars/urls.py
+++ backend/apps/cars/urls.py
@@ -3,4 +3,7 @@ from views import CarListCreate
 
 urlpatterns = [
     path('', CarListCreate.as_view(), name='car-list-create'),
-]
+6   path('api/cars/', views.get_cars, name='get_cars'),
+    path('admin/', admin.site.urls),
+6   path('cars/', include('cars.urls')),
+7]
