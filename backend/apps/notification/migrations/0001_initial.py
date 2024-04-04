735274989e5780cba4d3bafb40d491b3640b1264
Hryhorii_Struk <136825012+jnjijij@users.noreply.github.com>
2024-04-04 21:28:12 +0200

    Update 0001_initial.py

--- backend/apps/notification/migrations/0001_initial.py
+++ backend/apps/notification/migrations/0001_initial.py
@@ -14,10 +14,7 @@ class Migration(migrations.Migration):
         migrations.Notification(
             name='Notification',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=20)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
             ],
             options={
                 'db_table': 'auto_parks',
